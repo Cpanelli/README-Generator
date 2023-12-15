c6164a6689f5bf45d90ad0b2cd0729d987072bd3
Cpanelli <133393733+Cpanelli@users.noreply.github.com>
2023-12-15 01:26:55 -0500

    updated my code so it now works

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ReadMe-Generator
 
+# README-Generator [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+AS A developer I WANT a README generator SO THAT I can quickly create a professional README for a new project
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contact](#contact)
+
+## Installation
+Go to my repo. Open up in VS code. Open 
+
+## Usage
+Go to my repo. Open up in VS code. Open terminal in VS code. Type in  node index .js
+
+## License
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) License.
+
+
+
+## Contact
+- **Name:** Christopher Panelli
+- **Email:** cpanelli89@gmail.com
+- **GitHub:** [Cpanelli](https://github.com/Cpanelli)
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Apache 2.0") {
+    return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "BSD 3-Clause") {
+    return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "Apache 2.0") {
+    return "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "BSD 3-Clause") {
+    return "https://opensource.org/licenses/BSD-3-Clause";
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `## License
+This project is licensed under the [${license}](${renderLicenseLink(license)}) License.
+
+`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title} ${renderLicenseBadge(data.license)}
 
-`;
+## Description
+${data.description}
+
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [License](#license)
+4. [Contact](#contact)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contact
+- **Name:** ${data.name}
+- **Email:** ${data.email}
+- **GitHub:** [${data.github}](https://github.com/${data.github})
+  `;
 }
 
 module.exports = generateMarkdown;
